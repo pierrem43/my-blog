d4958bcdeb25f0323d7d148da692df8eefe852bc
jinane <zekji@hotmail.com>
2019-11-08 09:57:37 +0100

    forms and user accounts

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
@@ -10,5 +10,9 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
     body = models.TextField()
+
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self)])
